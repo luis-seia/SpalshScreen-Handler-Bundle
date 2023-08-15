d1d837934434f769f37686b69a6822e894066063
Luis-Seia <luis.seiax@gmail.com>
2023-08-15 22:12:20 +0200

    configured flashscreen

---
+++ app/src/main/java/com/example/spalshscreen_handler_bundle/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.spalshscreen_handler_bundle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/spalshscreen_handler_bundle/SplashScreen.kt
@@ -0,0 +1,18 @@
+package com.example.spalshscreen_handler_bundle
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreen , MainActivity::class.java))
+        },4000)
+    }
+}
